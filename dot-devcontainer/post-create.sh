24ea6c21962865bee63cbad18be0be5e7e627ff5
suzutomo <tomoya.suzuki@nex.nikkei.com>
2026-03-28 18:05:01 +0900

    Change owner of named volume dirs to node

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# change owner from root
+sudo chown -R node node_modules .pnpm-store
+
 # Ref: https://github.com/microsoft/vscode-dev-containers/blob/06b27a9a1d39dba6a7a8cca108cfb806c5c6e0d1/script-library/docs/node.md#using-nvm-from-a-dockerfile-or-postcreatecommand
 bash -i -c 'nvm install --lts'
 
