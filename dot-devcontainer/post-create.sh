c8871edaf1d5c27f0d3ade9a7ab08379bd4ae713
suzutomo <sztmsk@gmail.com>
2026-06-27 22:59:58 +0900

    chown node /home/node/.claude (#40)

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # change owner from root
-sudo chown -R node node_modules .pnpm-store
+sudo chown -R node node_modules .pnpm-store /home/node/.claude
 
 # Install tools by mise
 mise install
