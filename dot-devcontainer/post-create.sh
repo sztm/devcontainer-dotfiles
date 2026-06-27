f45c3da7d4b701a8db7b310de7563d1700ff49cc
suzutomo <sztmsk@gmail.com>
2026-06-27 22:37:48 +0900

    Handle mise config without dotfiles for container (#38)

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -4,10 +4,5 @@
 sudo chown -R node node_modules .pnpm-store
 
 echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
-ls -la /mise/config.toml
-ls -la /home/node/dotfiles/dot-mise/config.toml
-MISE_LOG_LEVEL=debug mise config 2>&1 | head -50
-echo $MISE_CONFIG_DIR
-echo $MISE_DATA_DIR
 mise doctor
 mise install
