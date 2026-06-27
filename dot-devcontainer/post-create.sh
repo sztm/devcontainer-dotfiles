e2e95a00ce598c4390151dd5a116fba38fec8da8
suzutomo <tomoya.suzuki@nex.nikkei.com>
2026-06-27 16:06:47 +0900

    Show debug log

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -4,7 +4,9 @@
 sudo chown -R node node_modules .pnpm-store
 
 echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
-pwd
-mise config
+ls -la /mise/config.toml
+MISE_LOG_LEVEL=debug mise config 2>&1 | head -50
+echo $MISE_CONFIG_DIR
+echo $MISE_DATA_DIR
 mise doctor
 mise install
