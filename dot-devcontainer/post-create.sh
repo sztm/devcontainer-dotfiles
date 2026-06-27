06790092b15693ac101ae1eb2116763a74643e17
suzutomo <tomoya.suzuki@nex.nikkei.com>
2026-06-27 22:59:32 +0900

    chown node /home/node/.claude

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # change owner from root
-sudo chown -R node node_modules .pnpm-store
+sudo chown -R node node_modules .pnpm-store /home/node/.claude
 
 echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
 mise doctor
