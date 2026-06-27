939f27650782ce1e4819bd7b2b570db09f5f9ed0
suzutomo <sztmsk@gmail.com>
2026-06-27 15:52:53 +0900

    Remove unused trust command (#33)

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -4,9 +4,5 @@
 sudo chown -R node node_modules .pnpm-store
 
 echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
-ls /mise
-ls -la ~/.local/state/mise/ignored-configs/
-mise trust /mise/config.toml
-ls -la ~/.local/state/mise/ignored-configs/
 mise doctor
 mise install
