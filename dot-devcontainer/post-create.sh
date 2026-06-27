276e69b39b6d9240bc56671168996966d30b7544
suzutomo <tomoya.suzuki@nex.nikkei.com>
2026-06-27 23:14:28 +0900

    Move pnpm-store to parent dir

--- dot-devcontainer/post-create.sh
+++ dot-devcontainer/post-create.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # change owner from root
-sudo chown -R node node_modules .pnpm-store /home/node/.claude
+sudo chown -R node node_modules /home/node/.claude /workspaces/.pnpm-store
+
+# Set the PNPM store directory to prevent making it in the project folder.
+pnpm config set store-dir /workspaces/.pnpm-store --location user
 
 # Install tools by mise
 mise install
